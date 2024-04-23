db9d3faf4e5fec183e96150884e4bccf54c8b0f3
s25508 <s25508@pjwstk.edu.pl>
2024-04-23 18:42:45 +0200

    Http Repository component

--- AnimaAPI/AnimaAPI/Program.cs
+++ AnimaAPI/AnimaAPI/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
+builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
+//builder.Services.AddScoped<IAnimalService, AnimalService>();
 var app = builder.Build();
 
 
